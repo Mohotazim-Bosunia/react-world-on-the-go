e39dbfa020d59863efc7a1b094332da0e29680b8
Mohotazim Hossen Bosunia <zimhossen0@gmail.com>
2026-04-18 20:30:07 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div className='counter'>
+            <h1>Name:Bangladesh</h1>
+        </div>
+    );
+};
+
+export default Countries;
